96101e30632ddd8546f850272f0c992a8705afdc
quelqu-1 <jeffrey.peterson95@gmail.com>
2022-01-11 21:56:08 -0500

    Finished game loop. Added mouse movement and click to release ball.

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
+// canvas
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+// ball
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-
-let dx = 2;
-let dy = -2;
-
+let ballMoving = false;
+let dx = 0;
+let dy = 0;
 let ballRadius = 10;
 
+// paddle
 let paddleHeight = 10;
 let paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
-
 let rightPressed = false;
 let leftPressed = false;
 
+// bricks
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+for (let c=0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r=0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+// score
+let score = 0;
+let winScore = (brickRowCount * brickColumnCount) * 100;
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-
+document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener("mouseup", mouseUpHandler, false);
 
 function keyDownHandler(e) {
     if (e.key == "Right" || e.key == "ArrowRight") {
@@ -34,6 +57,22 @@ function keyUpHandler(e) {
     } else if (e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
     }
+}
+
+function mouseMoveHandler(e) {
+    let relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
+        if (!ballMoving) {
+            x = relativeX - ballRadius / 2;
+        }
+    }
+}
+
+function mouseUpHandler(e) {
+        ballMoving = true;
+        dx = 2;
+        dy = -2;
 }
 
 function drawBall() {
@@ -52,10 +91,37 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for (let c=0; c < brickColumnCount; c++) {
+        for (let r=0; r < brickRowCount; r++) {
+            if (bricks[c][r].status == 1) {    
+                let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: " + score, 8, 20);
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
+    drawScore();
+    detectCollision();
 
 
     // check collisions with left and right walls
@@ -75,7 +141,6 @@ function draw() {
         }
     }
 
-
     if(rightPressed) {
         paddleX += 7;
         if (paddleX + paddleWidth > canvas.width){
@@ -90,6 +155,27 @@ function draw() {
 
     x += dx;
     y += dy;
+}
+
+function detectCollision() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            let b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score += 100;
+                    
+                    if (score == winScore) {
+                        alert("YOU WIN! CONGRATULATIONS!\nSCORE: " + score);
+                        document.location.reload();
+                        clearInterval(interval);
+                    }
+                }
+            }
+        }
+    }
 }
 
 let interval = setInterval(draw, 10);
